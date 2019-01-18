9788b1848c07953bc6edc4e3bd06b4ef0281e2f0
garkusha <Dfcbkmtdf1@>
2019-01-18 18:47:04 +0200

    finished module 4 Collections

--- src/main/java/module3/Main.java
+++ src/main/java/module3/Main.java
@@ -1,10 +1,8 @@
 package module3;
 
-import java.util.Scanner;
-
 public class Main {
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
         TryCatch.tryCatch();
         try {
             GenerateException.generateException();
--- src/main/java/module4/Collections/Main.java
+++ src/main/java/module4/Collections/Main.java
@@ -1,24 +1,44 @@
 package module4.Collections;
 
+import java.time.LocalDate;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-//        prog1();
-//        prog2();
-//        prog3();
-//        prog4();
-//        prog5();
-//        prog6();
-//        prog7();
-//        prog8();
-//        prog9();
-//        prog10();
-//        prog11();
-//        prog12();
+        prog1();
+        System.out.println("\nNext program");
+        prog2();
+        System.out.println("\nNext program");
+        prog3();
+        System.out.println("\nNext program");
+        prog4();
+        System.out.println("\nNext program");
+        prog5();
+        System.out.println("\nNext program");
+        prog6();
+        System.out.println("\nNext program");
+        prog7();
+        System.out.println("\nNext program");
+        prog8();
+        System.out.println("\nNext program");
+        prog9();
+        System.out.println("\nNext program");
+        prog10();
+        System.out.println("\nNext program");
+        prog11();
+        System.out.println("\nNext program");
+        prog12();
+        System.out.println("\nNext program");
         prog13();
-
+        System.out.println("\nNext program");
+        prog14();
+        System.out.println("\nNext program");
+        prog15();
+        System.out.println("\nNext program");
+        prog16();
+        System.out.println("\nNext program");
+        prog17();
 
     }
 
@@ -138,8 +158,9 @@ public class Main {
         list.add("rty");
         list.add("fgh");
 
-        System.out.println(list.size());
+        System.out.println("List size is " + list.size());
 
+        System.out.println("List content is ");
         for (String text : list) System.out.println(text);
 
     }
@@ -149,11 +170,13 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         int maxLength = 0;
 
+        System.out.println("Enter 5 different strings");
         for (int i = 0; i < 5; i++) {
             list.add(scan.nextLine());
             if (list.get(i).length() > maxLength) maxLength = list.get(i).length();
         }
 
+        System.out.println("Longest strings are:");
         for (String s : list) {
             if (s.length() == maxLength) System.out.println(s);
         }
@@ -165,11 +188,13 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         int minLength = 256;
 
+        System.out.println("Enter 5 different strings");
         for (int i = 0; i < 5; i++) {
             list.add(scan.nextLine());
             if (list.get(i).length() < minLength) minLength = list.get(i).length();
         }
 
+        System.out.println("Shortest strings are:");
         for (String s : list) {
             if (s.length() == minLength) System.out.println(s);
         }
@@ -180,8 +205,10 @@ public class Main {
         List<String> list = new ArrayList<>();
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("Enter 10 different strings");
         for (int i = 0; i < 10; i++) list.add(0, scan.nextLine());
 
+        System.out.println("Content of the List:");
         for (String s : list) System.out.println(s);
 
     }
@@ -190,6 +217,7 @@ public class Main {
         List<String> list = new ArrayList<>();
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("Enter 5 different strings");
         for (int i = 0; i < 5; i++) list.add(scan.nextLine());
 
         for (int i = 0; i < 13; i++) {
@@ -197,6 +225,7 @@ public class Main {
             list.remove(list.size() - 1);
         }
 
+        System.out.println("List after 13 transpositions");
         for (String s : list) System.out.println(s);
 
     }
@@ -204,7 +233,10 @@ public class Main {
     private static void prog12() {
         Set<String> set = new HashSet<>();
 
-        for (int i = 0; i < 20; i++) set.add("Л"+i);
+        for (int i = 0; i < 20; i++) set.add("Л" + i);
+
+
+        for (String s : set) System.out.println(s);
     }
 
     private static void prog13() {
@@ -215,8 +247,117 @@ public class Main {
 
         set.removeIf(integer -> integer > 10);
 
+        System.out.println("Set after removing all numbers above 10");
         for (Integer s : set) System.out.println(s);
 
+    }
+
+    private static void prog14() {
+        Map<String, String> map = new HashMap<String, String>();
+        Random random = new Random();
+        String lastName = "LastName7";
+        String firstName = "FirstName5";
+
+        while (map.size() < 10) map.put("LastName" + random.nextInt(20 + 1), "FirstName" + random.nextInt(20 + 1));
+
+        int counter = 0;
+
+        for (Map.Entry<String, String> pair : map.entrySet()) {
+            if (pair.getKey().equals(lastName) || pair.getValue().equals(firstName)) counter++;
+        }
+
+        System.out.println("Amount of people with entered name of lastname" + counter);
+    }
+
+    private static void prog15() {
+        Map<String, LocalDate> map = new HashMap<String, LocalDate>();
+        Random random = new Random();
+        LocalDate date;
+        String name;
+        Set<String> keysSet = new HashSet<String>();
+
+        while (map.size() < 10) {
+            name = "LastName" + random.nextInt(100 + 1);
+            date = LocalDate.of((random.nextInt(80) + 1935),
+                    (random.nextInt(12) + 1), (random.nextInt(28) + 1));
+            map.put(name, date);
+        }
+
+        for (Map.Entry<String, LocalDate> pair : map.entrySet()) {
+            String key = pair.getKey();
+            LocalDate value = pair.getValue();
+            keysSet.add(key);
+            System.out.println(key + " " + value);
+        }
+
+        System.out.println("\nAfter deleting all summer-born\n");
+
+        for (String key : keysSet) {
+            LocalDate value = map.get(key);
+
+            if (value.getMonthValue() > 5 && value.getMonthValue() < 9) {
+                map.remove(key);
+            } else System.out.println(key + " " + value);
+        }
+
+    }
+
+    private static void prog16() {
+        Map<String, String> map = new HashMap<String, String>();
+        Random random = new Random();
+        Set<String> keysSet = new HashSet<String>();
+
+        while (map.size() < 10) {
+            map.put("LastName" + random.nextInt(30 + 1), "FirstName" + random.nextInt(20 + 1));
+        }
+
+        for (Map.Entry<String, String> pair : map.entrySet()) {
+            String key = pair.getKey();
+            String value = pair.getValue();
+            System.out.println(key + " " + value);
+        }
+
+        for (Map.Entry<String, String> pair : map.entrySet()) {
+            for (Map.Entry<String, String> pairToCompare : map.entrySet()) {
+                if (!(pair.getKey().equals(pairToCompare.getKey())) && pair.getValue().equals(pairToCompare.getValue())) {
+                    keysSet.add(pair.getKey());
+                    keysSet.add(pairToCompare.getKey());
+                }
+            }
+        }
+
+        System.out.println("\nAfter deleting all clones\n");
+
+        for (String key : keysSet) map.remove(key);
+
+        for (Map.Entry<String, String> pair : map.entrySet()) {
+            String key = pair.getKey();
+            String value = pair.getValue();
+            System.out.println(key + " " + value);
+        }
+
+    }
+
+    private static void prog17() {
+        Map<String, String> map = new HashMap<String, String>();
+        Random random = new Random();
+
+        map.put("Иванов", "Иван");
+        map.put("Иванов1", "Александр");
+        map.put("Иванов2", "Пётр");
+        map.put("Иванов3", "Алексей");
+        map.put("Иванов4", "Иннокентий");
+        map.put("Иванов5", "Иван");
+        map.put("Иванов6", "Александр");
+        map.put("Иванов7", "Пётр");
+        map.put("Иванов8", "Алексей");
+        map.put("Иванов", "Иннокентий");
+
+        for (Map.Entry<String, String> pair : map.entrySet()) {
+            String key = pair.getKey();
+            String value = pair.getValue();
+            System.out.println(key + " " + value);
+        }
 
     }
 
